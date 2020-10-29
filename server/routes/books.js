8d62d7b8b8e1f4b36850dcfdf65613d91a0dc352
Liam Nelski <ljnelski@gmail.com>
2020-10-29 13:14:51 -0400

    Added Add and edit views, and Configured router to add and delete books

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,12 +4,12 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 // define the book model
-let book = require('../models/books');
+let Book = require('../models/books');
 
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  book.find( (err, books) => {
+  Book.find( (err, books) => {
     if (err) {
       return console.error(err);
     }
@@ -25,45 +25,104 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    res.render('books/add',
+    {
+        title: 'Add Book'
+    });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    let newBook = Book({
+        "Title" : req.body.Title,
+        "Description" : req.body.Description,
+        "Price" : req.body.Price,
+        "Author" : req.body.Author,
+        "Genre" : req.body.Genre
+    });
+    Book.create(newBook, (err, Book) =>
+    {
+        if (err)
+        {
+            //if there is an error creating a book, then log it
+            console.log(err);
+            res.end(err);
+        } 
+        else
+        {
+            // refresh list if there are no errors
+            res.redirect('/books');
+        }
+    });
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+router.get('edit/:id', (req, res, next) => {
+    id = req.params.id;
+    Book.findById(id, (err, bookToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // show the edit page
+            res.render('books/edit', 
+            {
+                title: 'Edit Book', 
+                book: bookToEdit
+            });
+        }
+    }); 
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('edit/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id;
+    
+    let updatedBook = Book ({
+        "_id": id,
+        "Title" : req.body.Title,
+        "Description" : req.body.Description,
+        "Price" : req.body.Price,
+        "Author" : req.body.Author,
+        "Genre" : req.body.Genre
+    });
+
+    Book.updateOne({_id: id}, updatedBook, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the books page
+            res.redirect('/books');
+        } 
+    });
 
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    let id = req.params.id
+
+    Book.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the movielist
+            res.redirect('/books');
+        } 
+    })
 });
 
 
